644afcb5bc21bc0e7ab4ce287f93a7a359e82148
irohasu2120 <sa1122photo@yahoo.co.jp>
2025-12-20 04:42:50 +0900

    add: 1st commit

---
+++ src/main/java/jp/sample/orepj/OrepjApplication.java
@@ -0,0 +1,13 @@
+package jp.sample.orepj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrepjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrepjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/sample/orepj/OrepjApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.sample.orepj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrepjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
